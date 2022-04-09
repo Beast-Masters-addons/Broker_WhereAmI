a3f1fa69ba3531cfd2171330944996f624e54b1f
datagutten <datagutten@datagutten.net>
2022-04-09 19:30:02 +0200

    Add correct dependencies at build

---
+++ files/yaml_merge.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+from yaml import load, dump
+from yaml import Loader, Dumper
+
+game = os.getenv('GAME_VERSION')
+main_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+
+with open(os.path.join(main_folder, '.pkgmeta'), 'r', encoding='utf8') as fp:
+    data = load(fp, Loader=Loader)
+
+    if game == 'classic':
+        data['externals']['libs/LibTouristClassicEra'] = {'url': 'https://repos.wowace.com/wow/libtourist-classic-era',
+                                                          'tag': 'latest'}
+    elif game == 'bcc':
+        data['externals']['libs/LibTouristClassic'] = {'url': 'https://repos.wowace.com/wow/libtourist-classic',
+                                                       'tag': 'latest'}
+    else:
+        data['externals']['libs/LibTourist-3.0'] = {'url': 'https://repos.wowace.com/wow/libtourist-3-0/trunk',
+                                                    'tag': 'latest'}
+
+with open(os.path.join(main_folder, '.pkgmeta'), 'w', encoding='utf8') as fp:
+    output = dump(data, Dumper=Dumper)
+    fp.write(output)
+
+shutil.copy(os.path.join(main_folder, 'files', game, 'Broker_WhereAmI.toc'),
+            os.path.join(main_folder, 'Broker_WhereAmI.toc'))
