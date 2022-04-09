65b6821b3f41a1cb21262611c6967895350db539
datagutten <datagutten@datagutten.net>
2022-04-09 16:32:41 +0200

    Python script to add LibTourist

---
+++ yaml_merge.py
@@ -0,0 +1,23 @@
+import os
+
+from yaml import load, dump
+from yaml import Loader, Dumper
+
+game = os.getenv('GAME_VERSION')
+with open('.pkgmeta', 'r', encoding='utf8') as fp:
+    data = load(fp, Loader=Loader)
+
+    if game == 'classic':
+        data['externals']['libs/LibTouristClassicEra'] = {'url': 'https://repos.wowace.com/wow/libtourist-classic-era',
+                                                          'tag': 'latest'}
+    elif game == 'bcc':
+        data['externals']['libs/LibTouristClassic'] = {'url': 'https://repos.wowace.com/wow/libtourist-classic',
+                                                       'tag': 'latest'}
+    else:
+        data['externals']['libs/LibTourist-3.0'] = {'url': 'https://repos.wowace.com/wow/libtourist-3-0/trunk',
+                                                    'tag': 'latest'}
+
+with open('.pkgmeta', 'w', encoding='utf8') as fp:
+    output = dump(data, Dumper=Dumper)
+    fp.write(output)
+    pass
