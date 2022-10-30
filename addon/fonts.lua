0774de5613c86929bebcb2c3ce07a876fa483c55
datagutten <datagutten@datagutten.net>
2022-10-30 20:49:24 +0100

    Add empty string argument after 10.0.0 change https://wowpedia.fandom.com/wiki/API_FontInstance_SetFont

--- addon/fonts.lua
+++ addon/fonts.lua
@@ -5,14 +5,14 @@ local fonts = addon.fonts
 ---Title Font, 14
 fonts.title = _G.CreateFont("titleFont")
 fonts.title:SetTextColor(1, 0.823529, 0, 1)
-fonts.title:SetFont(_G.GameTooltipText:GetFont(), 14)
+fonts.title:SetFont(_G.GameTooltipText:GetFont(), 14, "")
 
 ---Header Font, 12
 fonts.header = _G.CreateFont("headerFont")
 fonts.header:SetTextColor(0.92, 0.64, 0.37, 1)
-fonts.header:SetFont(_G.GameTooltipHeaderText:GetFont(), 13)
+fonts.header:SetFont(_G.GameTooltipHeaderText:GetFont(), 13, "")
 
 ---Regular Font, 12
 fonts.text = _G.CreateFont("textFont")
 fonts.text:SetTextColor(1, 0.823529, 0, 1)
-fonts.text:SetFont(_G.GameTooltipText:GetFont(), 12)
+fonts.text:SetFont(_G.GameTooltipText:GetFont(), 12, "")
